518c2e0295f5fac03b192a7a41b6202a2ad956b6
zelatndnjs <zelatndnjs@naver.com>
2024-10-28 18:29:53 +0900

    Feature (Application.java에 기존의 메소드들을 이용하여 프로그램 구현)

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,10 +2,28 @@ package racingcar;
 
 import java.util.List;
 
+import static camp.nextstep.edu.missionutils.Console.close;
 
 
 public class Application {
     public static void main(String[] args) {
+        try {
+            List<String> carNames = InputView.inputCarNames();
+            int tryCount = InputView.inputTryCount();
 
+            Game racingGame = new Game(carNames);
+
+            ResultView.printExecutionResult();
+            for (int i = 0; i < tryCount; i++) {
+                racingGame.moveCars();
+                ResultView.printRoundResult(racingGame.getCars());
+            }
+
+            List<String> winners = racingGame.getWinners();
+            ResultView.printWinners(winners);
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException();
+        }
+        close();
     }
 }
