28542659b49b56cc440348dca5dd03c231a9581c
zelatndnjs <zelatndnjs@naver.com>
2024-10-28 18:23:22 +0900

    Feature (ResultView.java에 라운드마다 차의 위치를 표시할 printRoundReslut 메소드 생성 및 -의 개수를 나타낼 getPositionString 메소드 생성)

--- src/main/java/racingcar/ResultView.java
+++ src/main/java/racingcar/ResultView.java
@@ -6,4 +6,19 @@ public class ResultView {
     public static void printExecutionResult() {
         System.out.println("\n실행 결과");
     }
+
+    public static void printRoundResult(List<Car> cars) {
+        for (Car car : cars) {
+            System.out.println(car.getName() + " : " + getPositionString(car.getPosition()));
+        }
+        System.out.println();
+    }
+
+    private static String getPositionString(int position) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < position; i++) {
+            sb.append("-");
+        }
+        return sb.toString();
+    }
 }
